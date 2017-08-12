208d9989f36e3475b264297971367bcef686372b
Qoli Wong <llqoli@gmail.com>
2017-08-12 13:39:06 +0800

    支持顯示 IP 列表

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,25 @@
 var num = 0;
 
 $(document).ready(function() {
-  setInterval(function() {
-    $('#ipList').load('/json');
-  }, 4000);
+
+  var ip = getUrlParam('ip');
+  console.log(ip);
+
+  if (ip != null) {
+    if ($("#ipList")[0]) {
+      setInterval(function() {
+        $('#ipList').load('/json/' + ip);
+      }, 4000);
+    }
+  }
+
+
+
 });
+
+function getUrlParam(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+  var r = window.location.search.substr(1).match(reg); //匹配目标参数
+  if (r != null) return unescape(r[2]);
+  return null; //返回参数值
+}
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,153 @@ var srequest = require('sync-request');
 var router = express.Router();
 var session = null;
 var stok = null;
+var url_prefix = "http://" + "10.0.0.1/cgi-bin/luci/;";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  get_stok(function(err, httpResponse, body) {
+    url = url_prefix + stok + "/api/sys/get_clients"
+    lg('請求網址', url);
+    request({
+      url: url,
+      headers: {
+        Cookie: session
+      }
+    }, function(error, response, body) {
+      json = JSON.parse(body);
+
+      console.log(json.clients);
+
+      res.render('list', {
+        title: 'IP Location',
+        stok: stok,
+        list: json.clients
+      });
+
+    });
+  })
+
+});
+
+router.get('/:ip', function(req, res) {
+
+  if (stok == null) {
+
+    error = {
+      status: 501,
+      stack: '找不到參數'
+    }
+
+    res.render('error', {
+      message: "stok 值不存在",
+      error: error
+    });
+
+    return
+  }
+
+  res.render('index', {
+    title: 'IP Location',
+    stok: stok
+  });
+});
+
+router.get('/json/:ip', function(req, res) {
+  var i = req.params.ip;
+  url = url_prefix + stok + "/api/sys/dump_conntrack?ipaddr=" + i;
+
+  lg('IP', i)
+  lg('請求網址', url)
+
+  if (stok != null) {
+
+    request({
+      url: url,
+      headers: {
+        Cookie: session
+      }
+    }, function(error, response, body) {
+      json = JSON.parse(body);
+
+      list = json.connections;
+
+      output = [];
+
+      for (var attributename in list) {
+        i = list[attributename];
+        l = getip_maxmind(i.dst);
+
+        if (l != null) {
+
+          if (l.hasOwnProperty('city')) {
+            c_name = l.city.names;
+          } else {
+            c_name = {
+              en: '-'
+            }
+          }
+
+          if (l.hasOwnProperty('country')) {
+            c_country = l.country.names;
+          } else {
+            c_country = {
+              'zh-CN': '-'
+            }
+          }
+
+          c = c_name['en'] + ' / ' + c_country['zh-CN'];
+        } else {
+          c = '-'
+        }
+
+        output[attributename] = {
+          state: i.state,
+          src: i.src,
+          sport: i.sport,
+          dst: i.dst,
+          local: c,
+          dport: i.dport,
+          proto: i.proto
+        }
+      }
+
+      res.render('ip', {
+        title: 'IP Location',
+        json: output
+      });
+    });
+  } else {
+
+    output[0] = {
+      local: 'stok 不存在'
+    }
+
+    res.render('ip', {
+      title: 'IP Location',
+      json: output
+    });
+  }
+});
+
+
+router.get('/get/:ip', function(req, res) {
+  res.json(getip_maxmind(req.params.ip));
+});
+
+module.exports = router;
+
+function getip_maxmind(i) {
+  var cityLookup = maxmind.openSync('./GeoLite2-City.mmdb');
+  var city = cityLookup.get(i);
+  return city;
+}
+
+function lg(name, text) {
+  console.log('> ' + name + ': ' + text);
+}
+
+function get_stok(callback) {
   request.post({
     url: 'http://10.0.0.1/cgi-bin/luci/web/index',
     form: {
@@ -18,108 +161,17 @@ router.get('/', function(req, res, next) {
     }
   }, function(err, httpResponse, body) {
 
-    session = httpResponse.headers['set-cookie']; //获取set-cookie字段值
-    j = JSON.stringify(session);
-    o = j.match(/stok=.{32}/g);
-    stok = o[0];
-    url = "http://" + "10.0.0.1/cgi-bin/luci/;" + stok + "/api/sys/dump_conntrack?ipaddr=10.0.0.203";
+    if (stok == null) {
+      session = httpResponse.headers['set-cookie']; //获取set-cookie字段值
+      j = JSON.stringify(session);
+      o = j.match(/stok=.{32}/g);
+      stok = o[0];
 
-    console.log(url);
-    console.log(stok);
+      lg('stok', stok);
+    }
 
-    res.render('index', {
-      title: 'IP Location',
-      stok: stok
-    });
+
+    callback(err, httpResponse, body);
 
   })
-
-});
-
-router.get('/json', function(req, res) {
-
-  console.log('> json...');
-
-  request({
-    url: url,
-    headers: {
-      Cookie: session
-    }
-  }, function(error, response, body) {
-    json = JSON.parse(body);
-
-    list = json.connections;
-
-    output = [];
-
-    for (var attributename in list) {
-      i = list[attributename];
-      l = getip_maxmind(i.dst);
-
-      if (l != null) {
-
-        if (l.hasOwnProperty('city')) {
-          c_name = l.city.names;
-        } else {
-          c_name = ' ';
-        }
-
-        if (l.hasOwnProperty('country')) {
-          c_country = l.country.names;
-        } else {
-          c_country = ' '
-        }
-
-        c = c_name['en'] + ' / ' + c_country['zh-CN'];
-      } else {
-        c = ''
-      }
-
-      output[attributename] = {
-        state: i.state,
-        src: i.src,
-        sport: i.sport,
-        dst: i.dst,
-        local: c,
-        dport: i.dport,
-        proto: i.proto
-      }
-    }
-
-    // console.log(output);
-
-    res.render('ip', {
-      title: 'IP Location',
-      json: output
-    });
-  });
-});
-
-
-
-router.get('/getip/:ip', function(req, res) {
-  var i = req.params.ip;
-  var ipq = srequest('GET', 'https://ipapi.co/' + i + '/json/');
-  ip = ipq.getBody();
-  ip = JSON.parse(ip);
-  res.json(ip);
-});
-
-router.get('/get/:ip', function(req, res) {
-  res.json(getip_maxmind(req.params.ip));
-});
-
-module.exports = router;
-
-function getip(i) {
-  var ipq = srequest('GET', 'https://ipapi.co/' + i + '/json/');
-  ip = ipq.getBody();
-  ip = JSON.parse(ip);
-  return ip
-}
-
-function getip_maxmind(i) {
-  var cityLookup = maxmind.openSync('./GeoLite2-City.mmdb');
-  var city = cityLookup.get(i);
-  return city;
 }
